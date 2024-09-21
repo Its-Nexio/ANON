2ee59718b7abede638efecf7481bfa73cb094c85
LucyXDbot <lucymorningstar.9783@gmail.com>
2024-09-21 18:16:59 +0530

    Update ping.py

--- AnonXMusic/plugins/tools/ping.py
+++ AnonXMusic/plugins/tools/ping.py
@@ -1,12 +1,11 @@
 from datetime import datetime
 from pyrogram import filters
-from pyrogram.types import Message
+from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
 
 from AnonXMusic import app
 from AnonXMusic.core.call import Anony
 from AnonXMusic.utils import bot_sys_stats
 from AnonXMusic.utils.decorators.language import language
-from AnonXMusic.utils.inline import supp_markup
 from config import BANNED_USERS, PING_IMG_URL
 
 
@@ -14,27 +13,21 @@ from config import BANNED_USERS, PING_IMG_URL
 @language
 async def ping_com(client, message: Message, _):
     start = datetime.now()
-    
-    # phale image ke sath reply kar rahe hai
+
+    # Pehle photo bhej rahe hain
     response = await message.reply_photo(
         photo=PING_IMG_URL,
         caption=_["ping_1"].format(app.mention),
     )
-    
-    # ping or system stats nikalna
+
+    # Ping aur system stats nikal rahe hain
     pytgping = await Anony.ping()
     UP, CPU, RAM, DISK = await bot_sys_stats()
-    
-    # calculating the response time
-    resp = (datetime.now() - start).microseconds / 1000
-    
-    # editing quotes with style
-    await response.edit_text(
-        _["ping_2"].format(resp, app.mention, UP, RAM, CPU, DISK, pytgping),
-        reply_markup=supp_markup(_),
-    )
 
-    # adding button to quotes style
+    # Response time calculate kar rahe hain
+    resp = (datetime.now() - start).microseconds / 1000
+
+    # Inline buttons create kar rahe hain
     buttons = [
         [
             InlineKeyboardButton("OVERALL STATS", callback_data="overall_stats"),
@@ -42,6 +35,7 @@ async def ping_com(client, message: Message, _):
         ],
     ]
     
+    # Caption ko edit kar rahe hain aur buttons add kar rahe hain
     await response.edit_caption(
         caption=_["ping_2"].format(resp, app.mention, UP, RAM, CPU, DISK, pytgping),
         reply_markup=InlineKeyboardMarkup(buttons),
