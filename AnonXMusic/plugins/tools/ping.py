1938cf5c13b72619344da4b3d723ff170cdd6b2e
LucyXDbot <lucymorningstar.9783@gmail.com>
2024-09-21 18:22:22 +0530

    Update ping.py

--- AnonXMusic/plugins/tools/ping.py
+++ AnonXMusic/plugins/tools/ping.py
@@ -1,11 +1,12 @@
 from datetime import datetime
 from pyrogram import filters
-from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.types import Message
 
 from AnonXMusic import app
 from AnonXMusic.core.call import Anony
 from AnonXMusic.utils import bot_sys_stats
 from AnonXMusic.utils.decorators.language import language
+from AnonXMusic.utils.inline import supp_markup
 from config import BANNED_USERS, PING_IMG_URL
 
 
@@ -13,30 +14,23 @@ from config import BANNED_USERS, PING_IMG_URL
 @language
 async def ping_com(client, message: Message, _):
     start = datetime.now()
-
-    # Pehle photo bhej rahe hain
+    
+    # Pehle image ke saath reply karo
     response = await message.reply_photo(
         photo=PING_IMG_URL,
         caption=_["ping_1"].format(app.mention),
     )
-
-    # Ping aur system stats nikal rahe hain
+    
+    # Ping aur system stats calculate karo
     pytgping = await Anony.ping()
     UP, CPU, RAM, DISK = await bot_sys_stats()
-
-    # Response time calculate kar rahe hain
     resp = (datetime.now() - start).microseconds / 1000
-
-    # Inline buttons create kar rahe hain
-    buttons = [
-        [
-            InlineKeyboardButton("OVERALL STATS", callback_data="overall_stats"),
-            InlineKeyboardButton("CLOSE", callback_data="close"),
-        ],
-    ]
     
-    # Caption ko edit kar rahe hain aur buttons add kar rahe hain
-    await response.edit_caption(
-        caption=_["ping_2"].format(resp, app.mention, UP, RAM, CPU, DISK, pytgping),
-        reply_markup=InlineKeyboardMarkup(buttons),
+    # Image ko delete karke, updated message ke saath edit karo
+    await response.delete()
+    
+    # Fir message ko stats ke saath update karo
+    await message.reply_text(
+        _["ping_2"].format(resp, app.mention, UP, RAM, CPU, DISK, pytgping),
+        reply_markup=supp_markup(_),
     )
