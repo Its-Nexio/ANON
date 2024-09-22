e5e82509f313da8681f6fe07644709281f4ffc3c
LucyXDbot <lucymorningstar.9783@gmail.com>
2024-09-22 12:21:53 +0530

    Update start.py

--- AnonXMusic/plugins/bot/start.py
+++ AnonXMusic/plugins/bot/start.py
@@ -46,42 +46,64 @@ async def start_pm(client, message: Message, _):
                 )
             return
         if name[0:3] == "inf":
-            m = await message.reply_text("🔎")
-            query = (str(name)).replace("info_", "", 1)
-            query = f"https://www.youtube.com/watch?v={query}"
-            results = VideosSearch(query, limit=1)
-            for result in (await results.next())["result"]:
-                title = result["title"]
-                duration = result["duration"]
-                views = result["viewCount"]["short"]
-                thumbnail = result["thumbnails"][0]["url"].split("?")[0]
-                channellink = result["channel"]["link"]
-                channel = result["channel"]["name"]
-                link = result["link"]
-                published = result["publishedTime"]
-            searched_text = _["start_6"].format(
-                title, duration, views, published, channellink, channel, app.mention
-            )
-            key = InlineKeyboardMarkup(
-                [
-                    [
-                        InlineKeyboardButton(text=_["S_B_9"], url=config.SUPPORT_CHAT),
-                        InlineKeyboardButton(text=_["S_B_8"], url=link),
-                    ],
-                ]
-            )
-            await m.delete()
-            await app.send_photo(
-                chat_id=message.chat.id,
-                photo=thumbnail,
-                caption=searched_text,
-                reply_markup=key,
-            )
-            if await is_on_off(2):
-                return await app.send_message(
-                    chat_id=config.LOGGER_ID,
-                    text=f"{message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ ᴛᴏ ᴄʜᴇᴄᴋ <b>ᴛʀᴀᴄᴋ ɪɴғᴏʀᴍᴀᴛɪᴏɴ</b>.\n\n<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>\n<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}",
-                )
+            await handle_info_request(client, message, name[4:], _)
+    else:
+        await send_welcome_message_with_image(client, message, _)
+
+async def send_welcome_message_with_image(client, message, _):
+    out = private_panel(_)
+    await message.reply_photo(
+        photo=random.choice(config.START_IMG_URL),
+        caption=_["start_2"].format(message.from_user.mention, app.mention),
+        reply_markup=InlineKeyboardMarkup(out),
+    )
+    if await is_on_off(2):
+        await app.send_message(
+            chat_id=config.LOGGER_ID,
+            text=f"{message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ.\n\n<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>\n<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}",
+        )
+
+async def handle_info_request(client, message, query, _):
+    m = await message.reply_text("🔎")
+    results = VideosSearch(f"https://www.youtube.com/watch?v={query}", limit=1)
+    for result in (await results.next())["result"]:
+        await format_and_send_video_info(client, message, result, _)
+    if await is_on_off(2):
+        await log_user_activity(client, message, "track information")
+
+async def format_and_send_video_info(client, message, video_info, _):
+    title = video_info["title"]
+    duration = video_info["duration"]
+    views = video_info["viewCount"]["short"]
+    thumbnail = video_info["thumbnails"][0]["url"].split("?")[0]
+    channellink = video_info["channel"]["link"]
+    channel = video_info["channel"]["name"]
+    link = video_info["link"]
+    published = video_info["publishedTime"]
+    searched_text = _["start_6"].format(
+        title, duration, views, published, channellink, channel, app.mention
+    )
+    key = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(text=_["S_B_9"], url=config.SUPPORT_CHAT),
+                InlineKeyboardButton(text=_["S_B_8"], url=link),
+            ],
+        ]
+    )
+    await m.delete()
+    await app.send_photo(
+        chat_id=message.chat.id,
+        photo=thumbnail,
+        caption=searched_text,
+        reply_markup=key,
+    )
+
+async def log_user_activity(client, message, activity):
+    await app.send_message(
+        chat_id=config.LOGGER_ID,
+        text=f"{message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ ᴛᴏ ᴄʜᴇᴄᴋ <b>{activity}</b>.\n\n<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>\n<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}",
+    )
     else:
         out = private_panel(_)
         await message.reply_photo(
