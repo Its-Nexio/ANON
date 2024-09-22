d78ac812618b5451646fe852523bcd57af0447d6
LucyXDbot <lucymorningstar.9783@gmail.com>
2024-09-22 14:59:41 +0530

    Update start.py

--- AnonXMusic/plugins/bot/start.py
+++ AnonXMusic/plugins/bot/start.py
@@ -96,8 +96,6 @@ async def start_pm(client, message: Message, _):
         await message.reply_photo(
             photo=random.choice(config.START_IMG_URL),  # First image block separately
         )
-        await message.reply_photo(
-            photo=random.choice(config.START_IMG_URL),
             caption=_["start_2"].format(message.from_user.mention, app.mention),
             reply_markup=InlineKeyboardMarkup(out),
         )
