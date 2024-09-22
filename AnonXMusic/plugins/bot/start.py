a5723a656e901df5ff1ecf8edb7e7847da4e97b0
LucyXDbot <lucymorningstar.9783@gmail.com>
2024-09-22 22:15:07 +0530

    Update start.py

--- AnonXMusic/plugins/bot/start.py
+++ AnonXMusic/plugins/bot/start.py
@@ -94,11 +94,11 @@ async def start_pm(client, message: Message, _):
         # First start_2 send photo with caption
         sticker_msg = await message.reply_sticker(
     "CAACAgUAAxkBAAEMwtFm2J-3UGAg_1SzGBH1GEug7AABZUYAAtMRAAJ_fjFWuigrmBCATJk2BA"
-)
+           )
+              await asyncio.sleep(5)  # the sticker will be deleted after 5 seconds
+              await sticker_msg.delete()  # delete the sticker message
 
-await asyncio.sleep(5)  # the sticker will be deleted after 5 seconds
-await sticker_msg.delete()  # delete the sticker message
-        await message.reply_photo(
+            await message.reply_photo(
             photo=random.choice(config.START_IMG_URL),
             caption=_["start_2"].format(message.from_user.mention),  # Pehla caption (start_2)
         )
