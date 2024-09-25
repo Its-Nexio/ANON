2965934cc85d0b877dbb4ac5ecb46083645f294b
LucyXDbot <lucymorningstar.9783@gmail.com>
2024-09-26 01:42:15 +0530

    Update thumbnails.py

--- AnonXMusic/utils/thumbnails.py
+++ AnonXMusic/utils/thumbnails.py
@@ -1,5 +1,6 @@
-from config import YOUTUBE_IMG_URL
 from youtubesearchpython.__future__ import VideosSearch
+
+from config import YOUTUBE_IMG_URL
 
 
 async def get_thumb(videoid):
@@ -11,14 +12,3 @@ async def get_thumb(videoid):
         return thumbnail
     except Exception as e:
         return YOUTUBE_IMG_URL
-
-
-async def get_qthumb(vidid):
-    try:
-        query = f"https://www.youtube.com/watch?v={vidid}"
-        results = VideosSearch(query, limit=1)
-        for result in (await results.next())["result"]:
-            thumbnail = result["thumbnails"][0]["url"].split("?")[0]
-        return thumbnail
-    except Exception as e:
-        return YOUTUBE_IMG_URLE_IMG_URL
