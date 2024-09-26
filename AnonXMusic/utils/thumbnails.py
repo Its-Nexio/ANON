857553b96b110df19fc18e235078a0b0d1691e57
LucyXDbot <lucymorningstar.9783@gmail.com>
2024-09-26 10:58:59 +0530

    Update thumbnails.py

--- AnonXMusic/utils/thumbnails.py
+++ AnonXMusic/utils/thumbnails.py
@@ -1,11 +1,9 @@
 import aiofiles
 import aiohttp
 import os
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 
-from AnonXMusic import app
-
-async def get_thumb(videoid, user_id):
+async def get_thumb(videoid, user_id, bot_name):
     thumbnail_url = f"https://img.youtube.com/vi/{videoid}/maxresdefault.jpg"
     save_path = f"cache/{videoid}_{user_id}.png"
     if os.path.isfile(save_path):
@@ -17,6 +15,15 @@ async def get_thumb(videoid, user_id):
                 f = await aiofiles.open(save_path, mode="wb")
                 await f.write(await response.read())
                 await f.close()
+
+                # Open the thumbnail image and type text on it
+                img = Image.open(save_path)
+                draw = ImageDraw.Draw(img)
+                font = ImageFont.truetype("arial.ttf", 20)  # Set the font and size as per your requirement.
+                text_width, text_height = draw.textsize(bot_name, font=font)
+                draw.text((img.width - text_width - 10, 10), bot_name, font=font, fill="white")
+
+                img.save(save_path)
                 return save_path
             else:
                 return None  # Here you need to add error handling if the thumbnail is not found.
